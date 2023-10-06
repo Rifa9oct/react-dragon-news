160c671e1afe1b5c97dd95978e074682607fb1c0
Rifa9oct <kohinuraktherr@gmail.com>
2023-10-06 18:37:51 +0600

    added breaking news and set date formate

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,14 +2,14 @@ import Header from "../Shared/Header/Header";
 import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
 import Navbar from "../Shared/Navbar/Navbar";
 import RightSideNav from "../Shared/RightSideNav/RightSideNav";
+import BreakingNews from "./BreakingNews";
+
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
-            <div className="bg-[#F3F3F3] p-4">
-                <button className="btn bg-[#D72050] hover:bg-[#D72050] text-white">Latest</button>
-            </div>
+            <BreakingNews></BreakingNews>
             <Navbar></Navbar>
             <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
                 <div className="border-4">
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,11 +1,12 @@
 import logo from "../../../assets/logo.png"
+import moment from 'moment';
 
 const Header = () => {
     return (
         <div className="mt-12 mb-8">
             <img className="mx-auto" src={logo}/>
             <h1 className='text-lg text-[#706F6F] text-center mt-3'>Journalism Without Fear or Favour</h1>
-            <p className="text-xl text-[#403F3F] font-medium text-center">Sunday, November 27, 2022</p>
+            <p className="text-xl text-[#403F3F] font-medium text-center">{moment().format("dddd,")} <span className="text-[#706F6F]">{moment().format("MMMM D, YYYY")}</span></p>
         </div>
     );
 };
